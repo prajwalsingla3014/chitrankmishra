0e1d908bd6adf24fefb04c2c3247b03b8476538c
chitrank0614 <37659873+chitrank0614@users.noreply.github.com>
2018-09-29 22:30:38 +0530

    Add files via upload

---
+++ extralong factorial.c
@@ -0,0 +1,55 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+void extraLongFactorials(int n) {
+    // Complete this function
+    int *num=malloc(40000000000000);
+    int temp=n;
+    int j=0;
+    int r=0;
+    int remain=0;
+    while(temp!=0)
+    {
+        r=temp%10;
+        temp=temp/10;
+        num[j]=r;
+        j++;
+    }
+    for(int i=n-1;i>0;i--)
+    {   temp=0;
+        int k=0;
+        while(k<j)
+        {
+            temp=i*num[k];
+            temp=temp+remain;
+            num[k]=temp%10;
+            temp=temp/10;
+            remain=temp;
+            k++;
+        }
+        while(remain!=0)
+        {
+            r=remain%10;
+            remain=remain/10;
+            num[j]=r;
+            j++;
+        }
+    }
+   for(int i=j-1;i>=0;i--)
+   {
+       printf("%d",num[i]);
+   }
+   system("pause");
+}
+
+int main() {
+    int n;
+    scanf("%i", &n);
+    extraLongFactorials(n);
+    return 0;
+}
