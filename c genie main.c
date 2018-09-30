6571c8bbe53556e7e093f14d114be96377da374e
prj1999 <38538265+prj1999@users.noreply.github.com>
2018-09-30 23:56:46 +0530

    Update c genie main.c

--- c genie main.c
+++ c genie main.c
@@ -1,3 +1,4 @@
+/* It is mind prediction games some random questions are asked and with answers of that questions result is analysed and output is predicted*/
 #include<stdio.h>
 #include<windows.h>
 #include<stdlib.h>
